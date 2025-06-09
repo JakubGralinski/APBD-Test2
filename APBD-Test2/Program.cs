874767f2110807789fd8e431de04e19d82fc5c2d
JakubGralinski <jakubm.gralinski@gmail.com>
2025-06-09 18:12:20 +0200

    ignore test

--- APBD-Test2/Program.cs
+++ APBD-Test2/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
         options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
